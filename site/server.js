cd1db17162329696339070805c1c7818ed11b9f9
Matthew Gadd <mattgadd86@googlemail.com>
2016-01-08 14:10:13 +0000

    add emojify

--- site/server.js
+++ site/server.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var app = express();
 var nunjucks = require("nunjucks");
+var config = require("../config");
+var ContribCat = require("../lib");
+var contribCat = new ContribCat(config);
+
 var port = 9000;
+var contibs;
 
 nunjucks.configure("./templates", {
 	autoescape: true,
@@ -10,14 +15,20 @@ nunjucks.configure("./templates", {
 });
 
 app.use(express.static('./'));
+app.use("/emojify", express.static("../node_modules/emojify.js/dist"));
+
 app.engine("html", nunjucks.render);
 app.set("view engine", "html");
 
 app.get("/:username", function(req, res) {
 	res.render('index.html', {
-		username : req.params.username
+		username : req.params.username,
+		contribs: contribs
   });
 });
 
-app.listen(port);
-console.log("Listening on port %s...", port);
+contribCat.run().then(function (results) {
+	contribs = results;
+	app.listen(port);
+	console.log("Listening on port %s...", port);
+});
