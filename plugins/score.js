1c9120dac3fdf965ab12cecba9c9e089b536b0ba
Matthew Gadd <mattgadd86@googlemail.com>
2016-01-08 14:27:09 +0000

    count emojis

--- plugins/score.js
+++ plugins/score.js
@@ -5,6 +5,14 @@ module.exports = function (options) {
 			var forScore = user.for.length * options.weighting.for;
 			var againstScore = user.against.length * options.weighting.against;
 			user.score = againstScore + forScore;
+			user.emojis = 0;
+
+			user.for.forEach(function (comment) {
+				var match = comment.body.match(/\:\w+\:/g);
+				if (match && match.length) {
+					user.emojis += match.length;
+				}
+			});
 		});
 		return results;
 	};
