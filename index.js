bcdf4d5187f577224dc2ab92c3739dc9d0e76213
agubler <agubler@gmail.com>
2016-01-16 22:18:55 +0000

    fix up index.js to run load --> sync.

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ var mongoose = require('mongoose');
 var ContribCat = require("./lib");
 var contribCat = new ContribCat(config);
 
-mongoose.connect("mongodb://localhost/contribcat");
 
-contribCat.run()
-	.then(contribCat.runReporters.bind(contribCat))
-	.finally(function () {
+console.log("Started Load");
+contribCat.load()
+	.then(contribCat.sync.bind(contribCat))
+	.then(() => {
+		console.log("Load Completed");
+	})
+	.finally(() => {
 		mongoose.disconnect();
 	});
