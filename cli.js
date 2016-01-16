7751a46066d9fa7c2737ea6c64abbd90ed2dd3cd
agubler <agubler@gmail.com>
2016-01-16 22:16:06 +0000

    basic cli

--- cli.js
+++ cli.js
@@ -6,30 +6,25 @@ var mongoose = require('mongoose');
 var config = require("./config");
 var ContribCat = require("./lib");
 
-
 program
 	.version("0.0.1")
-	.option("-m, --mode <mode>", "cli mode", /^(sync|report)$/i, "stats")
-	.option("-d, --days <number>", "cli mode", "Number of days to sync", 1)
+	.option("-m, --mode <mode>", "cli mode", /^(load|sync)$/i, "stats")
 	.parse(process.argv);
 
 var mode = program.mode.toLowerCase();
-
-if (mode === "sync") {
-
-} else if (mode === "report") {
-
-} else {
-	//invalid mode
-}
-
-
 var contribCat = new ContribCat(config);
 
-
-if (program.mode === "sync") {
-	contribCat.sync().then(function() {
-		console.log("process completed");
+if (mode === "load") {
+	contribCat.load().then(function() {
+		console.log("load completed");
 		mongoose.disconnect();
 	});
+} else if (mode === "sync") {
+	contribCat.sync().then(function() {
+		console.log("sync completed");
+		mongoose.disconnect();
+	});
+} else {
+	mongoose.disconnect();
+	console.error("unsupported mode")
 }
