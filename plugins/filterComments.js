4abd6b9aa86bdc3797e888adf81b608d7736b4ea
agubler <agubler@gmail.com>
2016-02-11 17:42:29 +0000

    exclude emoji only comments

--- plugins/filterComments.js
+++ plugins/filterComments.js
@@ -2,17 +2,18 @@
 module.exports = function (options) {
 
 	function filterComment(comment) {
+		let commentLength = comment.body.replace(/(:.*?:)*/g, "").length;
 		if (options.filterIssueOnly) {
-			return comment.path || comment.body.replace(/(:.*?:)*/g, "").length > options.minlength;
+			return (commentLength && comment.path) || commentLength > options.minlength;
 		}
-		return comment.body.replace(/(:.*?:)*/g, "").length > options.minlength;
+		return commentLength > options.minlength;
 	}
 
 	return function (results) {
 		results.users.forEach((user) => {
 			user.repos.forEach((repo) => {
-				repo.againstFiltered = repo.against.filter((comment) => filterComment);
-				repo.forFiltered = repo.for.filter((comment) => filterComment);
+				repo.againstFiltered = repo.against.filter(filterComment);
+				repo.forFiltered = repo.for.filter(filterComment);
 			});
 		});
 		return results;
