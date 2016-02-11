a1572a90ab8a74cc4c15f0796a978ec778727fbe
agubler <agubler@gmail.com>
2016-02-11 17:17:03 +0000

    clone repos before splicing

--- plugins/comfortableRepositories.js
+++ plugins/comfortableRepositories.js
@@ -14,9 +14,10 @@ module.exports = function (results) {
 		}).reverse();
 
 		let bestRepoCount = user.repos.length < 3 ? 1 : user.repos.length < 5 ? 2 : 3;
+		let clonedRepos = _.clone(user.repos);
 
-		user.strongestRepos = user.repos.splice(0, bestRepoCount);
-		user.weakestRepos = _.takeRight(user.repos, bestRepoCount).reverse();
+		user.strongestRepos = clonedRepos.splice(0, bestRepoCount);
+		user.weakestRepos = _.takeRight(clonedRepos, bestRepoCount).reverse();
 	});
 
 	return results;
