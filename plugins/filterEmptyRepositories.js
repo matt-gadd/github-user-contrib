c77569fc307e749ac3ef6739d5233d3cbed46b05
agubler <agubler@gmail.com>
2016-02-12 11:20:21 +0000

    extend filterEmptyRepositories and filterUsers

--- plugins/filterEmptyRepositories.js
+++ plugins/filterEmptyRepositories.js
@@ -1,10 +1,16 @@
 "use strict";
-module.exports =  function(results) {
-	results.users.forEach((user) => {
-		user.repos = user.repos.filter((repo) => {
-			return !!(repo.prs.length !== 0 || repo.for.length !== 0 || repo.against.length !== 0);
+module.exports = function(options) {
+	let forCommentKey = "for", againstCommentKey = "against";
+		if (options.useFilteredComments) {
+			forCommentKey = "forFiltered";
+			againstCommentKey = "againstFiltered";
+	}
+	return function (results) {
+		results.users.forEach((user) => {
+			user.repos = user.repos.filter((repo) => {
+				return !!(repo.prs.length !== 0 || repo[forCommentKey].length !== 0 || repo[againstCommentKey].length !== 0);
+			});
 		});
-	});
-
-	return results;
+		return results;
+	};
 };
--- plugins/filterUsers.js
+++ plugins/filterUsers.js
@@ -3,15 +3,19 @@
 module.exports = function (options) {
 	options = options || {};
 	options.excludes = options.excludes || [];
+	options.inactive = options.inactive || {};
 	return function (results) {
 		results.users = results.users.filter((user) => {
 			if (options.excludes.indexOf(user.name) !== -1) {
 				return false;
 			}
-			if (options.filterInactive) {
-				if (user.details.suspended_at) {
+			if (options.inactive.filter) {
+				if (!options.inactive.suspendedOnly || user.details.suspended_at) {
 					return user.repos.some(repo => repo.prs.length > 0 || repo.for.length > 0 || repo.against.length > 0);
 				}
+			}
+			if (options.filterSuspended) {
+				return !user.details.suspended_at;
 			}
 			return true;
 		});
