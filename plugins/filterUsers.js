f5a1eb93e3465138c76c1a1d1156bebcebde5044
agubler <agubler@gmail.com>
2016-02-16 17:17:25 +0000

    Support saving comment sub document

--- plugins/filterUsers.js
+++ plugins/filterUsers.js
@@ -10,7 +10,7 @@ module.exports = function (options) {
 			if (options.excludes.indexOf(user.name) !== -1) {
 				filtered = true;
 			} else if (options.filterSuspended) {
-				filtered = user.details.suspended_at;
+				filtered = !!user.details.suspended_at;
 			} else if (options.inactive.filter) {
 				if (!options.inactive.suspendedOnly || user.details.suspended_at) {
 					filtered = !user.repos.some(repo => repo.prs.length > 0 || repo.for.length > 0 || repo.against.length > 0);
